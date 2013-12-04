0fe1290f2bf8c7bcd221e74a4a8c30f45183e4a0
Hajo Eichler <hajo.eichler@commercetools.de>
2013-12-04 12:44:03 +0100

    Add base component.js for elastic.io.

---
+++ component.js
@@ -0,0 +1,11 @@
+{
+  "title": "sphere-product-xml-import",
+  "description": "Creates or updates your SPHERE.IO products from XML",
+  "actions": {
+    "services": {
+      "main":"./lib/xmlimport.js",
+      "title": "Product XML Import"
+    }
+  }
+}
+
